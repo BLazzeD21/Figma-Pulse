27ad95dda9bab5a931ab37566de961ba11a9a719
Alexandr Sekerin <blazzed21@gmail.com>
2023-07-18 17:59:19 +0300

    Added slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  $(".slider__inner").slick();
+  $(".slider__inner").slick({
+    speed: 1000,
+    slidesToShow: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    prevArrow: `<button type="button" class="slick-prev">
+    <img src = "../icons/arrows/prev-arrow.png">
+    </button>`,
+    nextArrow: `<button type="button" class="slick-next">
+    <img src = "../icons/arrows/next-arrow.png">
+    </button>`,
+  });
 });
