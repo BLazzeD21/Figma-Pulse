48c781b66ba18ee1f6edecb0c84c345a971198d4
Alexandr Sekerin <blazzed21@gmail.com>
2023-07-18 19:44:52 +0300

    Added responsive settings for slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
   $(".slider__inner").slick({
-    speed: 1000,
+    speed: 2000,
     slidesToShow: 1,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 3000,
     prevArrow: `<button type="button" class="slick-prev">
-    <img src = "../icons/arrows/prev-arrow.png">
+    <img src = "icons/arrows/prev-arrow.png">
     </button>`,
     nextArrow: `<button type="button" class="slick-next">
-    <img src = "../icons/arrows/next-arrow.png">
+    <img src = "icons/arrows/next-arrow.png">
     </button>`,
+    responsive: [
+      {
+        breakpoint: 575,
+        settings: {
+          arrows: false,
+          dots: true,
+        },
+      },
+    ],
   });
 });
