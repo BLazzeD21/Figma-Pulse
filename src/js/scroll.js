7b5aa1531896a5bf680a1a8058562f8bfe6001fd
Alexandr Sekerin <blazzed21@gmail.com>
2023-07-22 16:59:12 +0300

    Added pageup button to scroll the page to the first section

---
+++ src/js/scroll.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1000) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+});
